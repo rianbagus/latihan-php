fdeb71166216feda028cf5758da73296b8aacd78
rianbagus <rianmihawk@gmail.com>
2017-01-06 15:51:26 +0700

    latihan_html5_css

---
+++ html/latihan_html5_css/pengenalan.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pengenalan HTML 5 dan CSS</title>
+	<link rel="stylesheet" type="text/css" href="gaya.css">
+</head>
+<body>
+<header>
+	<!-- Ini merupakan sebuah bagian paling atas dari sebuah content -->
+	<!-- selector -->
+	<h1>Belajar HTML 5 dan CSS</h1>
+</header>
+
+<nav>
+	<ul>
+		<li><a href="#">HTML</li></a>
+		<li><a href="#">CSS</li></a>
+		<li>PHP</li>
+		<li>JAVASCRIPT</li>
+	</ul>	
+</nav>
+<section>
+	<article>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</article>
+	<article>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</article>
+	<article>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</article>
+</section>
+	
+<footer>
+	<p>&copy - 2017 - Rian</p>
+</footer>
+
+</body>
+</html>
